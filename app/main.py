7d8f64ff8844c728f5a4e4ae4647411b59d57526
Jose Gabriel Verutti Narvaez <65360184+JoseVerutti@users.noreply.github.com>
2021-08-17 20:06:54 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, Form, UploadFile, Depends, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from app.models import models
+#from .config.config import app_name
+#from app.config.database import SessionLocal, engine
 
-app = FastAPI()
+#models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI(
+    title = f"",
+    description = "MotsiApp",
+    version = "0.1"
+)
 
 @app.get('/inicio')
 async def ruta_de_prueba():
