1410d65ddfe537c6ff81c7ef87803d4eb191f0c8
Jose Gabriel Verutti Narvaez <65360184+JoseVerutti@users.noreply.github.com>
2021-08-17 21:15:28 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -8,6 +8,32 @@ from app.config.database import SessionLocal, engine
 
 app = FastAPI()
 
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+@app.get("/")
+def read_root():
+    return {"Motsi Backend" : "It's alive!!!"}
+
+# def get_db():
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
+
+from app.api import endpoints_call
+# import sys
+
 @app.get('/inicio')
 async def ruta_de_prueba():
     return 'por finnnnn'
