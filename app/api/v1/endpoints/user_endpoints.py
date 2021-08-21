d720ea43013e6c680ac34bcb65b23922e25e6d55
Gianpier Verutti <gianpierverutti@live.com>
2021-08-21 00:06:45 -0500

    12:06 User try

--- app/api/v1/endpoints/user_endpoints.py
+++ app/api/v1/endpoints/user_endpoints.py
@@ -14,28 +14,29 @@ def get_user(
 ):
     db_user = db.query(User).all()
     return(db_user )
+
 #------------ Get User (se optiene el usuario por medio del id)-----------------------------
 
-# @app.get("/api/v1/get_users/", tags=["Users"], response_model=List[schemas.User])
-# def get_user(
-#     id: int, 
-#     db: Session = Depends(get_db)
-# ):
+@app.get("/api/v1/get_users/", tags=["Users"], response_model=List[schemas.User])
+def get_user(
+    id: int, 
+    db: Session = Depends(get_db)
+):
 
-#     return user_providers.get_user(id, db)
+    return user_providers.get_user(id, db)
 
-# @app.post("/api/v1/create_user/", tags=["Users"])
-# def create_user(
-#     user: schemas.UserCreate,
-#     db: Session = Depends(get_db)
-# ):
+@app.post("/api/v1/create_user/", tags=["Users"])
+def create_user(
+    user: schemas.UserCreate,
+    db: Session = Depends(get_db)
+):
 
-#     return user_providers.create_user(user, db)
+    return user_providers.create_user(user, db)
 
-# @app.put("/api/v1/update_user/", tags=["User"])
-# def update_user(
-#     user: schemas.UserUpdate,
-#     db: Session = Depends(get_db)
-#     ):
+@app.put("/api/v1/update_user/", tags=["User"])
+def update_user(
+    user: schemas.UserUpdate,
+    db: Session = Depends(get_db)
+    ):
 
-#     return user_providers.update_user(user, db)
+    return user_providers.update_user(user, db)
