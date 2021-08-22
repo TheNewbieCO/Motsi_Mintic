39d6658501891fdfec2b22604f3f9f2d8a0bb459
Gianpier Verutti <gianpierverutti@live.com>
2021-08-22 17:39:04 -0500

    5:38pm OAuth try
    cambio de nombre de tabla a user2 y tags para swagger

--- app/api/v1/endpoints/auth_endpoints.py
+++ app/api/v1/endpoints/auth_endpoints.py
@@ -8,15 +8,18 @@ from typing import List, Optional
 import bcrypt
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
-@app.post("/token")
+@app.post("/token", tags=["OAuth2"])
 async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
-    user_dict = db.query(users).filter_by(name = form_data.username)
+    user_dict = db.query(user2).filter_by(name = form_data.username)
+    print('-'*10,user_dict)
     if not user_dict:
-            raise HTTPException(status_code=400, detail="Incorrect username or password")
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
 
     salt = bcrypt.gensalt()
     hashed_password_form = bcrypt.hashpw(form_data.password, salt)
+    print('-'*10,hashed_password_form)
     hashed_password_bd = user_dict['password']
+    print('-'*10,hashed_password_bd)
     if bcrypt.checkpw(hashed_password_form, hashed_password_bd):
 #    if not hashed_password_form == hashed_password_bd:
         raise HTTPException(status_code=400, detail="Incorrect username or password")
