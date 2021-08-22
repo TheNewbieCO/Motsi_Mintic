278f92c8dab93d076bca5761737ce3e1c5887869
Gianpier Verutti <gianpierverutti@live.com>
2021-08-22 17:25:55 -0500

    5:25pm auth try

--- app/api/endpoints_call.py
+++ app/api/endpoints_call.py
@@ -1,2 +1,3 @@
 from app.api.v1.endpoints import activity_enpoints
 from app.api.v1.endpoints import user_endpoints
+from app.api.v1.endpoints import auth_endpoints
---
+++ app/api/v1/endpoints/auth_endpoints.py
@@ -0,0 +1,26 @@
+from fastapi.params import Depends
+from fastapi import Depends,FastAPI,HTTPException,status
+from app.main import app, get_db
+from app.api.v1.providers import activity_providers
+from sqlalchemy.orm import Session
+from app.schemas import schemas
+from typing import List, Optional
+import bcrypt
+
+@app.post("/token")
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    user_dict = db.query(users).filter_by(name = form_data.username)
+    if not user_dict:
+            raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    salt = bcrypt.gensalt()
+    hashed_password_form = bcrypt.hashpw(form_data.password, salt)
+    hashed_password_bd = user_dict['password']
+    if bcrypt.checkpw(hashed_password_form, hashed_password_bd):
+#    if not hashed_password_form == hashed_password_bd:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+        print("match")
+
+    return {"access_token": user.username, "token_type": "bearer"}
+
+
