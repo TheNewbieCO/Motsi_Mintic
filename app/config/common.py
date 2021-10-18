521f0e5d4d55222a80004098a0fa42a984508037
Jose Gabriel Verutti Narvaez <65360184+JoseVerutti@users.noreply.github.com>
2021-10-18 15:56:46 -0500

    Update common.py

--- app/config/common.py
+++ app/config/common.py
@@ -1,3 +1,4 @@
+from fastapi.exceptions import HTTPException
 from fastapi.params import Depends
 from fastapi.security import OAuth2PasswordBearer, oauth2
 import jwt
@@ -11,12 +12,12 @@ SECRET_KEY = 'Motsi-Mintic-Backend-Airbnb2008'
 
 oauth2_schema= OAuth2PasswordBearer(tokenUrl="/api/v1/auth/")
 
-def create_access_token(user,days=30):
+def create_access_token(user,days=12):
 
     data={
         "user_id":user.id_user,
         "username":user.email,
-        "exp":datetime.utcnow() + timedelta(seconds=days)
+        "exp":datetime.utcnow() + timedelta(hours=days)
     }
     return jwt.encode(data, SECRET_KEY, algorithm="HS256")
 
@@ -25,8 +26,11 @@ def create_access_token(user,days=30):
 
 
 def decode_access_token(token):
-    return jwt.decode(token, SECRET_KEY, algorithms="HS256")
 
+    try:
+        return jwt.decode(token, SECRET_KEY, algorithms="HS256")
+    except Exception as err:
+        return None
 
 
 
@@ -34,7 +38,8 @@ def decode_access_token(token):
 def get_current_user ( token: str = Depends(oauth2_schema), db: Session = Depends(get_db) ) ->User :
 
     data= decode_access_token(token)
-
-    db_user = db.query(User).filter(User.id_user == data["user_id"]).first()
-    
-    return db_user
+    if data:
+        db_user = db.query(User).filter(User.id_user == data["user_id"]).first()
+        return db_user
+    else:
+        raise HTTPException(401, "La sesión ha expirado")
